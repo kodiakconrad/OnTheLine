da950c86afc9398af3780146345c7b60e669fb1a
Kodiak Conrad <kconrad1@uw.edu>
2019-04-17 22:00:51 -0700

    Initial Commit

---
+++ OnTheLine/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  OnTheLine
+//
+//  Created by Kodiak Conrad on 4/17/19.
+//  Copyright © 2019 Kodiak Conrad. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
