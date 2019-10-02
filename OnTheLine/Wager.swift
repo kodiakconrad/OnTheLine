03745e342b3024902411ebd65c65d2f7d484f642
Kodiak Conrad <kconrad1@uw.edu>
2019-10-02 12:31:57 -0700

    wager logic flow created

--- OnTheLine/Wager.swift
+++ OnTheLine/Wager.swift
@@ -8,8 +8,81 @@
 
 import Foundation
 
+enum EventType {
+    case game, statistic, outcome
+}
+
 class Event {
     var name: String!
+    var choiceA: String!
+    var choiceB: String!
+}
+
+class Game: Event {
+    var homeTeam: String!
+    var awayTeam: String!
+    var time: Date!
+    var spread: Double! // always with respect to home team
+    init(name: String, homeTeam: String, awayTeam: String, time: Date, spread: Double) {
+        super.init()
+        self.name = name
+        self.homeTeam = homeTeam
+        self.awayTeam = awayTeam
+        self.time = time
+        self.spread = spread
+        self.choiceA = homeTeam
+        self.choiceB = awayTeam
+        
+    }
+    
+    func setTime(time: Date) {
+        self.time = time
+    }
+    
+    func setSpread(spread: Double) {
+        self.spread = spread
+    }
+}
+
+class Statistic: Event {
+    var subject: String!
+    var statType: String!
+    var value: Double!
+    
+    init(name: String, subject: String, statType: String, value: Double) {
+        super.init()
+        self.name = name
+        self.subject = subject
+        self.statType = statType
+        self.value = value
+        self.choiceA = "Over"
+        self.choiceB = "Under"
+    }
+    
+    func setSubject(subject: String) {
+        self.subject = subject
+    }
+    
+    func setStatType(statType: String) {
+        self.statType = statType
+    }
+    
+    func setValue(value: Double) {
+        self.value = value
+    }
+}
+
+class Outcome: Event {
+    var option1: String!
+    var option2: String!
+    init(name: String, option1: String, option2: String) {
+        super.init()
+        self.name = name
+        self.option1 = option1
+        self.option2 = option2
+        self.choiceA = option1
+        self.choiceB = option2
+    }
 }
 
 enum Status {
