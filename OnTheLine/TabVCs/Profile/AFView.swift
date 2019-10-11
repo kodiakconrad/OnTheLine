765e6cae25e4ad6b75a5e247c98365509ed42966
Kodiak Conrad <kconrad1@uw.edu>
2019-10-11 15:58:58 -0700

    continued work on friends view

---
+++ OnTheLine/TabVCs/Profile/AFView.swift
@@ -0,0 +1,22 @@
+//
+//  AFView.swift
+//  OnTheLine
+//
+//  Created by Kodiak Conrad on 10/10/19.
+//  Copyright © 2019 Kodiak Conrad. All rights reserved.
+//
+
+import UIKit
+import PureLayout
+
+class AFView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- OnTheLine/TabVCs/Profile/AddFriendVC.swift
+++ OnTheLine/TabVCs/Profile/AddFriendVC.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 import Firebase
+import PureLayout
 
 
 class AddFriendVC: TabViewController {
 
     @IBOutlet weak var searchedUsername: UITextField!
+    var namelabel = UILabel.newAutoLayout()
+    var addButton = UIButton.newAutoLayout()
+    var profile = UIImageView.newAutoLayout()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         
         // Do any additional setup after loading the view.
     }
@@ -26,14 +31,17 @@ class AddFriendVC: TabViewController {
 
     @IBAction func search(_ sender: Any) {
         self.fetchUser()
+        
     }
-    func fetchUser() {
+    
+    func fetchUser()  {
         let userRef = db.collection("Users").document(searchedUsername.text!)
         userRef.getDocument { (document, error) in
             if let document = document, document.exists {
                 let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
                 print("Document data: \(dataDescription)")
                 self.getView(document: document)
+
                 
             } else {
                 print("Document does not exist")
@@ -42,12 +50,22 @@ class AddFriendVC: TabViewController {
         }
     }
     
-    private func getView(document: DocumentSnapshot) {
-        var friend = AddFriendView(frame: CGRect.zero)
+    func getView(document: DocumentSnapshot) {
+        let friend = AddFriendView(frame: CGRect.zero)
         let data = document.data()
-        let fnData = data?["firstname"] as? [String: String]
-        let lnData = data?["lastname"] as? [String: String]
+        print(type(of: data))
+        let fn = data!["firstname"]
+        let ln = data!["lastname"]
+        print(fn!)
+        print(ln!)
+        friend.nameLabel.text = "\(String(describing: fn)) + \(String(describing: ln))"
+        self.view.addSubview(friend)
         
+
+    }
+    
+    func addNewView(to container: UIView) {
+        let newView = UIView()
     }
     
 }
